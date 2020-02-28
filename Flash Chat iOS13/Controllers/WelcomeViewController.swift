e6a76b90f90cf0a425d2f6e7640836df2dd6ffbc
Shivaansh Prasann <shivaanshseal@gmail.com>
2020-02-27 20:48:01 -0500

    Cocoapod added

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+//import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    //@IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //titleLabel.text = "⚡️TrashChat"
+        
+        titleLabel.text = ""
+        var charIndex = 0
+        for letter in "⚡️TrashChat"{
+            Timer.scheduledTimer(withTimeInterval: 0.05 * Double(charIndex), repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
        
     }
     
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your projec
+platform :ios, '13.0' #This is minimum iOS version supported
+
+target 'Flash Chat iOS13' do
+  # Pods for Flash Chat iOS13
+
+  pod 'CLTypingLabel', '~> 0.4.0'
+
+end
