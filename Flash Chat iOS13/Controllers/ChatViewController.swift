238f4cf93285db59a0e1a1785aae3dadf5f1da36
Shivaansh Prasann <shivaanshseal@gmail.com>
2020-02-29 18:38:31 -0500

    Custom message bubble implemented

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -27,6 +27,9 @@ class ChatViewController: UIViewController {
         //hide the back button when this view is loaded
         navigationItem.hidesBackButton = true
         
+        //register custom cell
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
         
 
     }
@@ -55,8 +58,8 @@ extension ChatViewController: UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
         return cell
     }
     
